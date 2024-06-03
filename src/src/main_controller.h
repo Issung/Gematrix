66b0f4942ee54c7ff870d7fe68ae61494ec26cb8
Issung <joel@sonofgoran.com>
2024-06-03 20:27:10 +1000

    Refactor main_controller update function to use new change_state and change_menu functions

--- src/src/main_controller.h
+++ src/src/main_controller.h
@@ -20,7 +20,142 @@ private:
     menu<menu_option_key> play_menu = menu<menu_option_key>("PLAY", &main_menu);
     menu<menu_option_key> pause_menu = menu<menu_option_key>("PAUSE");
     menu<menu_option_key>* current_menu = &main_menu;
+
+    // Set to `nullptr` to hide menu.
+    void change_menu(menu<menu_option_key>* new_menu)
+    {
+        current_menu = new_menu;
+        selected_index = 0;
+
+        if (new_menu == nullptr)
+        {
+            for (auto o : menu_options_sprites)
+            {
+                for (auto s : o)
+                {
+                    s.set_visible(false);
+                }
+            }
+        }
+        else
+        {
+            // TODO: Draw menu title.
+            // TODO: Can optimise by only redrawing sprites when menu is different from last frame.
+            for (int i = 0; i < current_menu->options.max_size(); ++i)
+            {
+                menu_options_sprites[i].clear();
+            }
+
+            for (int i = 0; i < current_menu->options.size(); ++i)
+            {
+                text_generator.generate(0, -20 + (i * 20), current_menu->options[i].text, menu_options_sprites[i]);
+            }
+        }
+    }
+
+    void change_state(game_state new_state)
+    {
+        state = new_state;
+
+        if (state == game_state::menus)
+        {
+            change_menu(&main_menu);
+        }
+        else if (state == game_state::paused)
+        {
+            bc.hide();
+            change_menu(&pause_menu);
+        }
+        else if (state == game_state::ingame)
+        {
+            change_menu(nullptr);
+            bc.show();
+        }
+    }
+
+    void update_menus()
+    {
+        for (int i = 0; i < current_menu->options.size(); ++i)
+        {
+            auto palette = i == selected_index ? palette_highlight : palette_grey;
+            for (auto s : menu_options_sprites[i])
+            {
+                s.set_palette(palette);
+            }
+        }
+
+        if (bn::keypad::up_pressed() && selected_index > 0)
+        {
+            selected_index -= 1;
+        }
+        else if (bn::keypad::down_pressed() && selected_index < (current_menu->options.size() - 1))
+        {
+            selected_index += 1;
+        }
+        else if (bn::keypad::start_pressed() && state == game_state::paused)    // Start button shortcut for resume when paused.
+        {
+            change_state(game_state::ingame);
+        }
+        else if (bn::keypad::a_pressed())
+        {
+            auto key = current_menu->options[selected_index].key;
+            
+            if (key == menu_option_key::play)
+            {
+                change_menu(&play_menu);
+            }
+            else if (key == menu_option_key::sprint)    // TODO: Make game modes do something different.
+            {
+                bc.reset();
+                change_state(game_state::ingame);
+            }
+            else if (key == menu_option_key::resume)
+            {
+                change_state(game_state::ingame);
+            }
+            else if (key == menu_option_key::restart)
+            {
+                bc.reset();
+                change_state(game_state::ingame);
+            }
+            else if (key == menu_option_key::quit)
+            {
+                change_state(game_state::menus);
+            }
+        }
+        else if (bn::keypad::b_pressed())
+        {
+            if (current_menu->previous_menu != nullptr)
+            {
+                change_menu(current_menu->previous_menu);
+            }
+        }
+    }
+
 public:
+    void update()
+    {
+        if (state == game_state::ingame)
+        {
+            bc.update();
+
+            if (bn::keypad::start_pressed())
+            {
+                change_state(game_state::paused);
+            }
+        }
+        else if (state == game_state::menus || state == game_state::paused)
+        {
+            update_menus();
+        }
+        else
+        {
+            BN_ASSERT(false, "Invalid game state");
+        }
+
+        bn::core::update();
+    }
+
     main_controller()
     {
         bc.hide();
@@ -42,140 +177,7 @@ public:
         pause_menu.options.push_back(menu_option("RESUME", menu_option_key::resume));
         pause_menu.options.push_back(menu_option("RESTART", menu_option_key::restart));
         pause_menu.options.push_back(menu_option("QUIT", menu_option_key::quit));
-    }
 
-    void update()
-    {
-        if (state == game_state::menus)
-        {
-            // TODO: Can optimise by only redrawing sprites when menu is different from last frame.
-            for (int i = 0; i < current_menu->options.max_size(); ++i)
-            {
-                menu_options_sprites[i].clear();
-            }
-
-            for (int i = 0; i < current_menu->options.size(); ++i)
-            {
-                text_generator.generate(0, -20 + (i * 20), current_menu->options[i].text, menu_options_sprites[i]);
-
-                auto palette = i == selected_index ? palette_highlight : palette_grey;
-                for (auto s : menu_options_sprites[i])
-                {
-                    s.set_palette(palette);
-                }
-            }
-
-            if (bn::keypad::up_pressed() && selected_index > 0)
-            {
-                selected_index -= 1;
-            }
-            else if (bn::keypad::down_pressed() && selected_index < (current_menu->options.size() - 1))
-            {
-                selected_index += 1;
-            }
-            else if (bn::keypad::start_pressed() && current_menu == &pause_menu)    // Start button shortcut for resume.
-            {
-                // TODO: This code is repeated from `resume` case below.
-                state = game_state::ingame;
-                bc.show();
-
-                for (auto o : menu_options_sprites)
-                {
-                    for (auto s : o)
-                    {
-                        s.set_visible(false);
-                    }
-                }
-            }
-            else if (bn::keypad::a_pressed())
-            {
-                auto key = current_menu->options[selected_index].key;
-                
-                if (key == menu_option_key::play)
-                {
-                    current_menu = &play_menu;
-                    selected_index = 0;
-                }
-                else if (key == menu_option_key::sprint)
-                {
-                    bc.reset();
-                    bc.show();
-                    state = game_state::ingame;
-
-                    for (auto o : menu_options_sprites)
-                    {
-                        for (auto s : o)
-                        {
-                            s.set_visible(false);
-                        }
-                    }
-                }
-                else if (key == menu_option_key::resume)
-                {
-                    state = game_state::ingame;
-                    bc.show();
-
-                    for (auto o : menu_options_sprites)
-                    {
-                        for (auto s : o)
-                        {
-                            s.set_visible(false);
-                        }
-                    }
-                }
-                else if (key == menu_option_key::restart)
-                {
-                    bc.reset();
-                    bc.show();
-                    state = game_state::ingame;
-
-                    for (auto o : menu_options_sprites)
-                    {
-                        for (auto s : o)
-                        {
-                            s.set_visible(false);
-                        }
-                    }
-                }
-                else if (key == menu_option_key::quit)
-                {
-                    state = game_state::menus;
-                    current_menu = &main_menu;
-                }
-            }
-            else if (bn::keypad::b_pressed())
-            {
-                if (current_menu->previous_menu != nullptr)
-                {
-                    current_menu = current_menu->previous_menu;
-                }
-            }
-        }
-        else if (state == game_state::ingame)
-        {
-            bc.update();
-
-            if (bn::keypad::start_pressed())
-            {
-                bc.hide();
-                current_menu = &pause_menu;
-                selected_index = 0;
-                state = game_state::menus;
-            }
-        }
-        //else if (state == game_state::paused)
-        //{
-        //    if (bn::keypad::start_pressed())
-        //    {
-        //        bc.show();
-        //        state = game_state::ingame;
-        //    }
-        //}
-        else
-        {
-            BN_ASSERT(false, "Invalid game state");
-        }
-
-        bn::core::update();
+        change_menu(&main_menu);
     }
 };

cf24078050432031ddf5837bf142485fde1bc330
Issung <joel@sonofgoran.com>
2024-07-28 18:18:26 +1000

    Rename lots of variables within game_controller to improve clarity

--- src/src/game_controller.h
+++ src/src/game_controller.h
@@ -65,11 +65,11 @@ enum class game_state
 
 enum class gameover_state
 {
-    waiting_for_board_to_stop,  // Input disbled, wait for the board drawer animation to stop, then greyout the board.
-    waiting_for_board_to_greyout,   // 1 second greying out the board gems one by one.
-    waiting_for_score_counter_to_stop,  // Wait for displayed_score to reach the actual score, then greyout all text.
-    waiting_for_text_to_greyout,  // Period for greying out all text on the right of the game screen.
-    waiting_for_menu_to_appear, // Short period after the score counter stops, 1 second wait, before showing the gameover/hiscore menu.
+    board_finishing_animation,  // Input disbled, wait for the board drawer animation to stop, then greyout the board.
+    greying_out_board,   // 1 second greying out the board gems one by one.
+    waiting_for_score_counter,  // Wait for displayed_score to reach the actual score, then greyout all text.
+    greying_out_text,  // Period for greying out all text on the right of the game screen.
+    small_pause_before_menu, // Short period after the score counter stops, 1 second wait, before showing the gameover/hiscore menu.
 };
 
 // Handles user input, passing it to `board`, and managing `board_drawer`'s animations, tracking score and combo.
@@ -81,14 +81,14 @@ private:
     background_controller& background;
     bn::affine_bg_ptr board_bg = bn::affine_bg_items::board.create_bg(11, 51);
     bn::sprite_text_generator text_generator = bn::sprite_text_generator(gj::fixed_32x64_sprite_font);
-    bn::vector<bn::sprite_ptr, 5> score_header_text;  // Text sprites that just say "SCORE".
-    bn::vector<bn::sprite_ptr, 5> combo_header_text;    // Text sprites that say "COMBO".
-    bn::vector<bn::sprite_ptr, 4> time_header_text;    // Text sprites that say "TIME".
-    bn::vector<bn::sprite_ptr, 5> goal_or_limit_header_text;    // Text sprites that say either "GOAL" or "LIMIT".
-    bn::vector<bn::sprite_ptr, 32> score_number_sprites;
-    bn::vector<bn::sprite_ptr, 4> combo_text_sprites;
-    bn::vector<bn::sprite_ptr, 5> timer_sprites;
-    bn::vector<bn::sprite_ptr, 5> goal_or_limit_text;   // Text sprites for the goal/limit value.
+    bn::vector<bn::sprite_ptr, 5> header_score;  // Header text sprites that say "SCORE".
+    bn::vector<bn::sprite_ptr, 5> header_combo;    // Header text sprites that say "COMBO".
+    bn::vector<bn::sprite_ptr, 4> header_time;    // Header text sprites that say "TIME".
+    bn::vector<bn::sprite_ptr, 5> header_goal_or_limit;    // Header text sprites that say either "GOAL" or "LIMIT".
+    bn::vector<bn::sprite_ptr, 32> value_score;
+    bn::vector<bn::sprite_ptr, 4> value_combo;
+    bn::vector<bn::sprite_ptr, 5> value_time;
+    bn::vector<bn::sprite_ptr, 5> value_goal_or_limit;   // Text sprites for the goal/limit value.
     bn::sprite_ptr spr_selector = bn::sprite_items::selector.create_sprite(0, 0);
     bn::sprite_ptr spr_selector_dirs = bn::sprite_items::selector_dir.create_sprite(0, 0);  // Selector sprite with direction arrows.
     bn::sprite_palette_ptr text_palette = gj::fixed_32x64_sprite_font.item().palette_item().create_palette();
@@ -107,13 +107,13 @@ private:
     int start_countdown_timer_frames = 0;    // How many frames remain until the game starts.
     bool board_animating = false;   // Is the board currently animating.
 
-    // Gameover animation state
-    gameover_state go_state;    // Current stage of the animation.
-    BOARD_ANIM board_anim;
-    int frames_waiting_for_board_to_greyout = 0;
-    int frames_waiting_for_score_to_finish_counting = 0;    // How many frames spent waiting for score to finish counting.
-    int frames_waiting_for_text_to_greyout = 0;
-    int frames_waiting_for_gameover_menu_to_appear = 0; // How many frames spent waiting for menu to appear after score finished counting.
+    // Gameover animation state, prefixed with `go_`.
+    const int go_frames_per_text_element = 6;
+    gameover_state go_state;    // Current stage of the gameover animation.
+    BOARD_ANIM go_board_greyout_anim;   // Animation order for the board greyout.
+    int go_frames_greying_out_board = 0;    // How many frames spent waiting for board to grey out.
+    int go_frames_greying_out_text = 0; // How many frames spent waiting for text elements to grey out.
+    int go_frames_menu_pause = 0; // How many frames spent waiting for menu to appear after score finished counting.
 
     // Sound items for matches, increasing in pitch, use higher numbers for combos.
     bn::sound_item match_sounds[MATCH_SOUNDS_LEN] = {
@@ -160,14 +160,14 @@ private:
 
     void greyout_text()
     {
-        for (auto& s : score_header_text) { s.set_palette(gameover_grey_text); }
-        for (auto& s : combo_header_text) { s.set_palette(gameover_grey_text); }
-        for (auto& s : time_header_text) { s.set_palette(gameover_grey_text); }
-        for (auto& s : goal_or_limit_header_text) { s.set_palette(gameover_grey_text); }
-        for (auto& s : goal_or_limit_text) { s.set_palette(gameover_grey_text); }
-        for (auto& s : timer_sprites) { s.set_palette(gameover_grey_text); }
-        for (auto& s : score_number_sprites) { s.set_palette(gameover_grey_text); }
-        for (auto& s : combo_text_sprites) { s.set_palette(gameover_grey_text); }
+        for (auto& s : header_score) { s.set_palette(gameover_grey_text); }
+        for (auto& s : header_combo) { s.set_palette(gameover_grey_text); }
+        for (auto& s : header_time) { s.set_palette(gameover_grey_text); }
+        for (auto& s : header_goal_or_limit) { s.set_palette(gameover_grey_text); }
+        for (auto& s : value_goal_or_limit) { s.set_palette(gameover_grey_text); }
+        for (auto& s : value_time) { s.set_palette(gameover_grey_text); }
+        for (auto& s : value_score) { s.set_palette(gameover_grey_text); }
+        for (auto& s : value_combo) { s.set_palette(gameover_grey_text); }
         for (auto& ft : floating_texts) { ft.set_palette(gameover_grey_text); }
     }
 
@@ -175,8 +175,8 @@ private:
     {
         int jiggle = (displayed_score < score && displayed_score % 2 == 0 ? 1 : 0);    // Jiggle text up and down each frame while displayed score is incrementing.
 
-        score_number_sprites.clear();
-        text_generator.generate(VALUE_X, -62 - jiggle, bn::to_string<32>(displayed_score), score_number_sprites);   // TODO: Fix Y position to align with gems border when added.
+        value_score.clear();
+        text_generator.generate(VALUE_X, -62 - jiggle, bn::to_string<32>(displayed_score), value_score);   // TODO: Fix Y position to align with gems border when added.
     }
 
     // Heart and soul of the game. Updates the board frame by frame to perform animations, once the animations complete
@@ -278,21 +278,20 @@ private:
         spr_selector.set_visible(false);
         spr_selector_dirs.set_visible(false);
         state = game_state::gameover;
-        board_anim = board_anims::get_random();
-        go_state = gameover_state::waiting_for_board_to_stop;
-        frames_waiting_for_board_to_greyout = 0;
-        frames_waiting_for_score_to_finish_counting = 0;
-        frames_waiting_for_text_to_greyout = 0;
-        frames_waiting_for_gameover_menu_to_appear = 0;
+        go_board_greyout_anim = board_anims::get_random();
+        go_state = gameover_state::board_finishing_animation;
+        go_frames_greying_out_board = 0;
+        go_frames_greying_out_text = 0;
+        go_frames_menu_pause = 0;
     }
 public: 
     // Constructor.
     game_controller(background_controller& _background) : background(_background)
     {
         text_generator.set_left_alignment();
-        text_generator.generate(HEADER_X, -71, "SCORE", score_header_text);   // TODO: Fix Y position to align with gems border when added.
-        text_generator.generate(HEADER_X, -50, "COMBO", combo_header_text);
-        text_generator.generate(HEADER_X, +40, "TIME", time_header_text);
+        text_generator.generate(HEADER_X, -71, "SCORE", header_score);   // TODO: Fix Y position to align with gems border when added.
+        text_generator.generate(HEADER_X, -50, "COMBO", header_combo);
+        text_generator.generate(HEADER_X, +40, "TIME", header_time);
         // LIMIT/GOAL text is generated in newgame function.
         text_generator.set_right_alignment();
 
@@ -323,18 +322,18 @@ public:
 
     void hide()
     {
-        for (auto& s : score_header_text) { s.set_visible(false); }
-        for (auto& s : combo_header_text) { s.set_visible(false); }
-        for (auto& s : time_header_text) { s.set_visible(false); }
-        for (auto& s : goal_or_limit_header_text) { s.set_visible(false); }
-        for (auto& s : goal_or_limit_text) { s.set_visible(false); }
-        for (auto& s : timer_sprites) { s.set_visible(false); }
+        for (auto& s : header_score) { s.set_visible(false); }
+        for (auto& s : header_combo) { s.set_visible(false); }
+        for (auto& s : header_time) { s.set_visible(false); }
+        for (auto& s : header_goal_or_limit) { s.set_visible(false); }
+        for (auto& s : value_goal_or_limit) { s.set_visible(false); }
+        for (auto& s : value_time) { s.set_visible(false); }
         for (auto& ft : floating_texts) { ft.set_visible(false); }
         if (countdown_number_sprite.has_value()) { countdown_number_sprite.value().set_visible(false); }
         
         board_bg.set_visible(false);
-        score_number_sprites.clear();
-        combo_text_sprites.clear();
+        value_score.clear();
+        value_combo.clear();
         spr_selector.set_visible(false);
         spr_selector_dirs.set_visible(false);
         bd.hide();
@@ -342,11 +341,11 @@ public:
 
     void show()
     {
-        for (auto& s : score_header_text) { s.set_visible(true); s.set_palette(text_palette); }
-        for (auto& s : combo_header_text) { s.set_visible(true); s.set_palette(text_palette); }
-        for (auto& s : time_header_text) { s.set_visible(true); s.set_palette(text_palette); }
-        for (auto& s : goal_or_limit_header_text) { s.set_visible(true); s.set_palette(text_palette); }
-        for (auto& s : goal_or_limit_text) { s.set_visible(true); s.set_palette(text_palette); }
+        for (auto& s : header_score) { s.set_visible(true); s.set_palette(text_palette); }
+        for (auto& s : header_combo) { s.set_visible(true); s.set_palette(text_palette); }
+        for (auto& s : header_time) { s.set_visible(true); s.set_palette(text_palette); }
+        for (auto& s : header_goal_or_limit) { s.set_visible(true); s.set_palette(text_palette); }
+        for (auto& s : value_goal_or_limit) { s.set_visible(true); s.set_palette(text_palette); }
         for (auto& ft : floating_texts) { ft.set_visible(true); ft.set_palette(text_palette); }
 
         board_bg.set_visible(true);
@@ -385,14 +384,14 @@ public:
         this->score_goal = mode == game_mode::sprint ? levels::sprint[level] : 0;
         this->time_limit_frames = mode == game_mode::timeattack ? levels::timeattack[level] : 0;
 
-        goal_or_limit_header_text.clear();
+        header_goal_or_limit.clear();
         text_generator.set_left_alignment();
-        text_generator.generate(HEADER_X, +61, mode == game_mode::sprint ? "GOAL" : "LIMIT", goal_or_limit_header_text);
+        text_generator.generate(HEADER_X, +61, mode == game_mode::sprint ? "GOAL" : "LIMIT", header_goal_or_limit);
         text_generator.set_right_alignment();
 
-        goal_or_limit_text.clear();
+        value_goal_or_limit.clear();
         auto goal_limit_value = mode == game_mode::sprint ? bn::to_string<5>(score_goal) : util::frames_to_time_string(time_limit_frames);
-        text_generator.generate(VALUE_X, +70, goal_limit_value, goal_or_limit_text);
+        text_generator.generate(VALUE_X, +70, goal_limit_value, value_goal_or_limit);
     }
 
     // Returns true if game is complete, based on different conditions depending on game mode.
@@ -400,7 +399,7 @@ public:
     {
         if (state == game_state::gameover)
         {
-            if (go_state == gameover_state::waiting_for_board_to_stop)
+            if (go_state == gameover_state::board_finishing_animation)
             {
                 update_board();
                 update_displayed_score();
@@ -408,18 +407,18 @@ public:
                 if (!board_animating)
                 {
                     greyout_board();
-                    go_state = gameover_state::waiting_for_board_to_greyout;
+                    go_state = gameover_state::greying_out_board;
                 }
             }
-            else if (go_state == gameover_state::waiting_for_board_to_greyout)
+            else if (go_state == gameover_state::greying_out_board)
             {
                 // This step waits 1 second (60 frames) while we wait for the gems on the board to greyout 1 by 1.
                 update_displayed_score();
 
-                ++frames_waiting_for_board_to_greyout;
+                ++go_frames_greying_out_board;
 
                 // There are 5*6 gems (30) so we use the time waiting on this step halved to find the index of the next gem.
-                auto number = frames_waiting_for_board_to_greyout / 2;
+                auto number = go_frames_greying_out_board / 2;
 
                 // Search through the animation to find the next gem to grey out.
                 int row = -1, col = -1;   // Set these to -1, so once they are set the loops break.
@@ -427,7 +426,7 @@ public:
                 {
                     for (int c = 0; c < board::cols && col == -1; ++c)
                     {
-                        if (board_anim[r][c] == number)
+                        if (go_board_greyout_anim[r][c] == number)
                         {
                             row = r;    // Setting these will break the loops.
                             col = c;
@@ -437,51 +436,51 @@ public:
 
                 bd.greyout(row, col);
 
-                if (frames_waiting_for_board_to_greyout == 59)
+                if (go_frames_greying_out_board == 59)
                 {
-                    go_state = gameover_state::waiting_for_score_counter_to_stop;
+                    go_state = gameover_state::waiting_for_score_counter;
                 }
             }
-            else if (go_state == gameover_state::waiting_for_score_counter_to_stop)
+            else if (go_state == gameover_state::waiting_for_score_counter)
             {
                 update_displayed_score();
 
                 if (displayed_score == score)
                 {
-                    go_state = gameover_state::waiting_for_text_to_greyout;
+                    go_state = gameover_state::greying_out_text;
                 }
             }
-            else if (go_state == gameover_state::waiting_for_text_to_greyout)
+            else if (go_state == gameover_state::greying_out_text)
             {
                 // 7*8 = 56
                 // 7 -> 14 -> 21 -> 28 -> 35 -> 42 -> 49 -> 56 -> 63
                 //const int text_elements = 8;    // header + value for score, combo, time, limit/goal.
-                const int frames_per_element = 6;
 
-                if (frames_waiting_for_text_to_greyout >= frames_per_element * 8)
-                    go_state = gameover_state::waiting_for_menu_to_appear;
-                else if (frames_waiting_for_text_to_greyout > frames_per_element * 7)
-                    for (auto s : score_header_text) s.set_palette(gameover_grey_text);
-                else if (frames_waiting_for_text_to_greyout > frames_per_element * 6)
-                    for (auto s : score_number_sprites) s.set_palette(gameover_grey_text);
-                else if (frames_waiting_for_text_to_greyout > frames_per_element * 5)
-                    for (auto s : combo_header_text) s.set_palette(gameover_grey_text);
-                else if (frames_waiting_for_text_to_greyout > frames_per_element * 4)
-                    for (auto s : combo_text_sprites) s.set_palette(gameover_grey_text);
-                else if (frames_waiting_for_text_to_greyout > frames_per_element * 3)
-                    for (auto s : time_header_text) s.set_palette(gameover_grey_text);
-                else if (frames_waiting_for_text_to_greyout > frames_per_element * 2)
-                    for (auto s : timer_sprites) s.set_palette(gameover_grey_text);
-                else if (frames_waiting_for_text_to_greyout > frames_per_element * 1)
-                    for (auto s : goal_or_limit_header_text) s.set_palette(gameover_grey_text);
-                else if (frames_waiting_for_text_to_greyout > 0)
-                    for (auto s : goal_or_limit_text) s.set_palette(gameover_grey_text);
+                if (go_frames_greying_out_text >= go_frames_per_text_element * 8)
+                    go_state = gameover_state::small_pause_before_menu;
+                else if (go_frames_greying_out_text > go_frames_per_text_element * 7)
+                    for (auto s : header_score) s.set_palette(gameover_grey_text);
+                else if (go_frames_greying_out_text > go_frames_per_text_element * 6)
+                    for (auto s : value_score) s.set_palette(gameover_grey_text);
+                else if (go_frames_greying_out_text > go_frames_per_text_element * 5)
+                    for (auto s : header_combo) s.set_palette(gameover_grey_text);
+                else if (go_frames_greying_out_text > go_frames_per_text_element * 4)
+                    for (auto s : value_combo) s.set_palette(gameover_grey_text);
+                else if (go_frames_greying_out_text > go_frames_per_text_element * 3)
+                    for (auto s : header_time) s.set_palette(gameover_grey_text);
+                else if (go_frames_greying_out_text > go_frames_per_text_element * 2)
+                    for (auto s : value_time) s.set_palette(gameover_grey_text);
+                else if (go_frames_greying_out_text > go_frames_per_text_element * 1)
+                    for (auto s : header_goal_or_limit) s.set_palette(gameover_grey_text);
+                else if (go_frames_greying_out_text > 0)
+                    for (auto s : value_goal_or_limit) s.set_palette(gameover_grey_text);
 
-                ++frames_waiting_for_text_to_greyout;
+                ++go_frames_greying_out_text;
             }
-            else if (go_state == gameover_state::waiting_for_menu_to_appear)
+            else if (go_state == gameover_state::small_pause_before_menu)
             {
-                if (++frames_waiting_for_gameover_menu_to_appear >= 6)
+                // Tiny pause matching the text element greyouts.
+                if (++go_frames_menu_pause >= go_frames_per_text_element)
                 {
                     return true;
                 }
@@ -615,12 +614,12 @@ public:
 
         update_displayed_score();
 
-        combo_text_sprites.clear();
-        text_generator.generate(VALUE_X, -41, bn::format<4>("x{}", combo), combo_text_sprites);
+        value_combo.clear();
+        text_generator.generate(VALUE_X, -41, bn::format<4>("x{}", combo), value_combo);
         
         auto time_str = util::frames_to_time_string(timer_frames);
-        timer_sprites.clear();
-        text_generator.generate(VALUE_X, +49, time_str, timer_sprites);
+        value_time.clear();
+        text_generator.generate(VALUE_X, +49, time_str, value_time);
 
         if (state == game_state::playing)
         {
